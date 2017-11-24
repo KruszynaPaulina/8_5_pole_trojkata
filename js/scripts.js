641fc3922f2ce8190741bf777ba40434f560325b
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2017-11-24 13:48:49 +0100

    Add task 8.5

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('Enter length triangle base "a"'),
+h = prompt('Enter triangle height "h"'),
+triangleArea; 
+
+triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
